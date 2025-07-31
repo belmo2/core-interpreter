f72d1e023bfc602d968c792c6f45eb2d30974a1f
belmo2 <belmo2@illinois.edu>
2025-07-31 18:02:06 -0400

    Update Interp.hs

--- Interp.hs
+++ Interp.hs
@@ -3,32 +3,43 @@
 module Interp where
 
 import qualified Data.HashMap.Lazy as M
-
 import Types
 
-eval :: Expr -> Env -> Int -- You will almost certainly need to change this to use your own Value type.
-eval (ENum i) _ = i
+--- ### Evaluation Monad and Environment
 
--- Use this function as your top-level entry point so you don't break `app/Main.hs`
+-- | The evaluation monad to report runtime errors
+type Eval a = Either String a
 
+--- ### Evaluator
+
+-- | The evaluator function
+-- Takes an expression and an environment, and evaluates it to a Value
+eval :: Expr -> Env -> Eval Value
+eval (ENum i) _ = return $ VInt i
+
+eval (EVar x) env =
+  case M.lookup x env of
+    Just v  -> return v
+    Nothing -> Left $ "Unbound variable: " ++ x
+
+-- TODO:
+-- - Implement EAp (function application)
+-- - Implement ELet and ELet True (let and letrec)
+-- - Implement ELam (lambdas)
+-- - Implement EPack (constructors)
+-- - Implement ECase (pattern matching)
+
+--- ### Top-Level Entry Point
+
+-- | Use this function as your top-level entry point so you don't break `app/Main.hs`
 run :: Core -> String
 run prog =
   case M.lookup "main" prog of
     Nothing -> error "Supercombinator main not defined."
-    Just (_,[],mainBody) ->
-      let result = eval mainBody (M.empty)
-       in show result
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
+    Just (_, [], mainBody) ->
+      case eval mainBody M.empty of
+        Right (VInt n) -> show n
+        Right _ -> error "Main did not return an integer."
+        Left err -> error $ "Runtime error: " ++ err
+    Just (_, args, _) ->
+      error $ "main should not have parameters, but got: " ++ show args
