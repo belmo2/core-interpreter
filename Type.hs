50f4d46e0f7e0c7f758685e0d8c84a371378286d
belmo2 <belmo2@illinois.edu>
2025-07-31 18:30:56 -0400

    Update Type.hs

--- Type.hs
+++ Type.hs
@@ -5,11 +5,10 @@ module Types where
 
 import qualified Data.HashMap.Lazy as M
 
-type Env = M.HashMap Name Int  -- You will probably need to change this
-
 type Name = String
 type IsRec = Bool
 
+-- | Core language expressions (AST)
 data Expr = EVar Name                  -- Variables
           | ENum Int                   -- Numbers
           | EPack Int Int              -- Constructors
@@ -24,6 +23,23 @@ data Expr = EVar Name                  -- Variables
           | ELam [Name] Expr           -- Functions (Lambdas)
    deriving (Eq, Show)
 
+-- | A top-level function declaration: name, parameters, and body
 type Decl = (Name, [Name], Expr)       -- The name, parameter list, and body of a supercombinator declaration
 
+-- | A Core program is a map of supercombinator names to their declarations
 type Core = M.HashMap Name Decl        -- A core program is an environment of declarations
+
+--- ### Runtime evaluation types
+
+-- | Values produced by evaluation
+data Value
+  = VInt Int                        -- Integer literal
+  | VFun (Value -> Eval Value)      -- Curried function (closure)
+  | VPack Int [Value]               -- Constructor tag + arguments
+  deriving (Eq)                     -- Add Show if you want debug print
+
+-- | Evaluation result monad with error reporting
+type Eval a = Either String a
+
+-- | Runtime environment: maps variable names to evaluated values
+type Env = M.HashMap Name Value
