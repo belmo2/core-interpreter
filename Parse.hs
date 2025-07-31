6e65523127e0e562a6e7d79f3e2f459f1bd1311e
belmo2 <belmo2@illinois.edu>
2025-07-31 17:26:19 -0400

    Update Parse.hs

--- Parse.hs
+++ Parse.hs
@@ -22,41 +22,80 @@ type Parser = ParsecT String () Identity
 
 --- ### Lexicals
 
+-- | Parses a specific symbol and consumes trailing spaces
 symbol :: String -> Parser String
 symbol s = do string s
               spaces
               return s
 
+-- | Parses a positive integer and consumes trailing spaces
 int :: Parser Int
 int = do digits <- many1 digit <?> "an integer"
          spaces
          return (read digits :: Int)
 
+-- | Parses a lowercase identifier and consumes trailing spaces
 id :: Parser Name
 id = do first <- oneOf ['a' .. 'z']
         rest <- many (oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "'_")
         spaces
         return $ first:rest
 
+--- ### Atomic Expressions
 
-  
+-- | Parses an integer expression
 eInt :: Parser Expr
 eInt = do i <- int
           return $ ENum i
-          
-expr :: Parser Expr
-expr = eInt
 
+-- | Parses a variable expression
+eVar :: Parser Expr
+eVar = do v <- id
+          return $ EVar v
+
+-- | Parses a constructor Pack expression: Pack{n,m}
+ePack :: Parser Expr
+ePack = do _ <- symbol "Pack"
+           _ <- symbol "{"
+           tag <- int
+           _ <- symbol ","
+           arity <- int
+           _ <- symbol "}"
+           return $ EPack tag arity
+
+-- | Parses a parenthesized expression
+eParen :: Parser Expr
+eParen = do _ <- symbol "("
+            e <- expr
+            _ <- symbol ")"
+            return e
+
+-- | Parses an atomic expression (variable, number, Pack, or parenthesized)
+aexpr :: Parser Expr
+aexpr = try ePack <|> try eInt <|> try eVar <|> eParen
+
+--- ### Expressions
+
+-- | Parses left-associative application expressions
+expr :: Parser Expr
+expr = do es <- many1 aexpr
+          return $ foldl1 EAp es
+
+--- ### Top-Level Declarations
+
+-- | Parses a single supercombinator declaration
 decl :: Parser Decl
 decl = do name <- id
           params <- many id
           _ <- symbol "="
           body <- expr
-          return (name,params,body)
+          return (name, params, body)
 
+-- | Parses a complete Core program (one or more declarations)
 core :: Parser Core
 core = do decls <- decl `sepBy` (symbol ";")
-          return $ M.fromList [(n,v) | v@(n,_,_) <- decls]
+          return $ M.fromList [(n, v) | v@(n, _, _) <- decls]
 
+-- | Entry point for parsing a Core program
 parseCore :: String -> Either ParseError Core
 parseCore text = parse core "Core" text
